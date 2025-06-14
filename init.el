6ef088da0f8a190a7a0844a912a31e3b539711a9
regularuser0 <hdelarue@proton.me>
2025-06-14 16:57:35 +0200

    rm name

--- init.el
+++ init.el
@@ -32,11 +32,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/home/hippolyte/.emacs.d/diary.org"
-     "/home/hippolyte/.emacs.d/config.org"))
  '(package-selected-packages
-   '(all-the-icons-ivy-rich async bongo catppuccin-theme company counsel
+   '(all-the-icons-ivy-rich async catppuccin-theme company counsel
 			    darkroom dashboard dired-preview dmenu
 			    doom-modeline elcord elfeed emms
 			    multiple-cursors nix-mode nyan-mode
