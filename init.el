4a6c4855df8cf117f605d1aa7868cd4fdab6d4b5
regularuser0 <hdelarue@proton.me>
2025-07-09 22:37:08 +0200

    update

--- init.el
+++ init.el
@@ -32,7 +32,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(all-the-icons-ivy-rich async avy catppuccin-theme company counsel
+			    darkroom dashboard dired-preview dmenu
+			    doom-modeline elcord elfeed emms
+			    multiple-cursors nix-mode nyan-mode
+			    org-modern org-present org-roam-ui
+			    popup-kill-ring rainbow-delimiters
+			    rainbow-mode sudo-edit sunshine
+			    switch-window tree-sitter-langs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
